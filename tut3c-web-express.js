422297b5e51830b4d8567c1df4cec9697147c6cf
Niko Köbler <niko@n-k.de>
2015-01-17 17:49:12 +0100

    deliver static files

--- tut3c-web-express.js
+++ tut3c-web-express.js
@@ -2,13 +2,13 @@ var http = require("http");
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var fs = require("fs");
 var vertx = require("vertx2-core");
 var app = express();
 
-app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
-app.post('/', function (req, res) {
+app.post('/api', function (req, res) {
     console.log("Received Message: %s", JSON.stringify(req.body));
     if (Object.getOwnPropertyNames(req.body).length > 0) {
         vertx.eventbus.send("tictactoe", req.body, function(message) {
@@ -19,17 +19,24 @@ app.post('/', function (req, res) {
     }
 });
 
-app.get('/init', function(req, res) {
+app.get('/api', function(req, res) {
     vertx.eventbus.send("init_ttt", {}, function(message) {
         res.json(message.body);
     });
 });
 
-/*app.get('/', function(req, res) {
-    var filename = path.join(__dirname, 'index.html');
-    console.log(filename);
-    res.sendFile(filename);
-});*/
+app.get('/:file', function(req, res) {
+    var filename = path.join(__dirname, "web", req.params.file);
+    fs.readFile(filename, "utf8", function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.set("Content-type", "text/html");
+            res.send(data);
+            res.end();
+        }
+    });
+});
 
 var server = app.listen(9000, function() {
     var host = server.address().address;
