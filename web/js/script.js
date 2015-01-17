d81cb0a45bf0823a9f99b4a11a52d3f2fec2325b
Stephanie Peters <s.peters@netbiscuits.com>
2015-01-17 17:09:25 +0100

    add a basic web interface

---
+++ web/js/script.js
@@ -0,0 +1,73 @@
+var currentPlayer = "X";
+
+$(document).ready(function () {
+  
+  $.each($('.board td'), function(index, value) {
+       $(value).on("click", function () {
+          console.log(getJsonFrom($(value)));
+          doMove(getJsonFrom($(value)));
+       });
+  });
+  
+  $('button').on("click", initBoard);
+  
+  initBoard();
+});
+
+var getJsonFrom = function(cell) {
+
+  return { player: currentPlayer, coord: [cell.attr("data-x"), cell.attr('data-y')]};
+};
+
+var updateBoard = function(json) {
+  $('.log').prepend($("<div>" + json.status + "</div>"));
+  $.each($('.board td'), function(index, value) {
+      var val = result.board[$(value).attr("data-x")][$(value).attr("data-y")];
+      if( val == null ) {
+          $(value).text('&nbsp;');
+      } else {
+          $(value).html(val);
+      }
+  });
+};
+
+var initBoard = function() {
+		$.ajax("/api" , {
+      contentType: "application/json",
+      error: function(xhr, status, error) {
+					$('.log').prepend($("<div>There was an error:" + status + "\n" + JSON.stringify(error) + "</div>"));
+    			console.log(error);
+      },
+       success: function(result, status, xhr) {
+          updateBoard(result);
+			},
+      type: "GET"
+      }
+    );
+};
+
+var doMove = function(req) {
+		$.ajax("/api" , {
+      contentType: "application/json",
+      error: function(xhr, status, error) {
+					$('.log').prepend($("<div>There was an error:" + status + "\n" + JSON.stringify(error) + "</div>"));
+    			console.log(error);
+      },
+       success: function(result, status, xhr) {
+          if(statusOkay(result.status)) {
+            updateBoard(result);
+            if (currentPlayer === "X") {
+              currentPlayer = "O";
+            } else {
+              currentPlayer = "X";
+            }
+          }
+			},
+      type: "POST"
+      }
+    );
+};
+
+var statusOkay = function(status) {
+  return status.indexOf("Illegal") < 0;
+} 
